86c1edd8571573496487b267d24e9387c3ab511e
BeneSchlz <123456789+odin@bschulz735@gmail.com>
2024-09-04 17:25:50 +0200

    Added script for opening and closing modal

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-ss
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".add-btn");
+const closeModalBtn = document.querySelector(".close-btn");
+
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+
+  openModalBtn.addEventListener("click", openModal);
+
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden")
+  };
+
+  closeModalBtn.addEventListener("click", closeModal);
+
+  overlay.addEventListener("click", closeModal);
